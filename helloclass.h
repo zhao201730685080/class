c0aa9949c42c16f406d07a7a558c64bd2f741a67
ZHAOFEIHONG <980733433@qq.com>
2018-06-21 16:19:50 +0800

    add my github code

---
+++ helloclass.h
@@ -0,0 +1,20 @@
+#ifndef HELLOCLASS_H
+#define HELLOCLASS_H
+
+#include<iostream>
+
+using namespace std;
+
+class HelloClass
+{
+public:
+
+    helloclass();
+    ~helloclass();
+    void sayHello();
+};
+
+
+#endif
+
+
